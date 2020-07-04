a88ea5051b09fb2d390920565e9bdab9b21fefc3
venkatt6 <venkatt760@gmail.com>
2020-07-03 23:39:43 -0400

    Add project files.

---
+++ Controllers/createController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_VT.Models;
+
+namespace MVC_VT.Controllers
+{
+    public class createController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(createUser user)
+        {
+            //var user = new User();            
+            return View(user);
+        }
+    }
+}
---
+++ Models/createUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_VT.Models
+{
+    public class createUser
+    {
+        [Required]
+        public string userId { get; set; }
+        [Required]
+        public string userName { get; set; }
+        [Required]
+        public string userDept { get; set; }
+        public string userLocation { get; set; }
+    }
+}
